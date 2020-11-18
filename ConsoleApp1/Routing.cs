afd1adc1375ab41266bc2a05cca125a3862a7d73
konrad3612 <konrad3612@gmail.com>
2020-11-18 20:37:37 +0100

    dunno

--- ConsoleApp1/Routing.cs
+++ ConsoleApp1/Routing.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp1
 {
     public class Routing
     {
-        public const int V_size = 100;
+        public const int V_size = 35;
         public List<int> W = new List<int>();
         public List<int> V = new List<int>();
         public List<Node> E = new List<Node>();
@@ -17,7 +17,8 @@ namespace ConsoleApp1
         public int vs = 0;
         public int vq = V_size;
         public Dictionary<int, List<int>> Neighbour = new Dictionary<int, List<int>>();
-        public int[] Prevoius = new int[V_size+1];
+        public int[] Previous = new int[V_size+1];
+        public List<int> Path = new List<int>();
 
         public double E_min;
         public double E_max;
@@ -31,7 +32,7 @@ namespace ConsoleApp1
         public double[] T_ = new double[V_size+1];                    //traffic load
         public double[] Tao = new double[V_size+1];                   //tao = T_i/T
         public double[] Lambda = new double[V_size+1];                //if energy left is less than e_min
-        public double[,] D = new double[V_size, V_size+1];            //distance
+        public double[,] D = new double[V_size+1, V_size+1];            //distance
         public double[,] Delta = new double[V_size+1, V_size+1];        //is in transmision range 0/1 distance <R
 
         public Dictionary<int, Coordinates> Coordinates = new Dictionary<int, Coordinates>();
@@ -39,16 +40,19 @@ namespace ConsoleApp1
         private void GenerateData()
         {
             E_min = 20;
-            E_max = 200;
-            R = 20;
+            E_max = 2000;
+            R = 18;
             ni = 0.3;
             Beta = 5;
-            Gamma = 3;
-            Q = 4;
-            for (int i = 1; i < V_size; i++)
+            Gamma = 20;
+            Q = 50000;
+            for (int i = 0; i < V_size + 1; i++)
             {
                 var randomGenerator = new Random();
                 p[i] = (randomGenerator.NextDouble() / 2) + 0.5;
+                //handicap dla node'ów po skosie
+                if ((i % 6 == i / 6) && i != 35 && i != 0)
+                    p[i] = 0.2;
                 E_nergy[i] = E_max;
                 Tao[i] = randomGenerator.NextDouble();
             }
@@ -70,32 +74,37 @@ namespace ConsoleApp1
 
         private void GenerateLocations()
         {
-            var randomGenerator = new Random();
-            for (int i = 1; i < V_size; i++)
+            //var randomGenerator = new Random(0);
+            for (int i = 0; i < V_size + 1; i++)
             {
+                //var coordinates = new Coordinates()
+                //{
+                //    X = randomGenerator.Next(0, 50),
+                //    Y = randomGenerator.Next(0, 50)
+                //};
                 var coordinates = new Coordinates()
                 {
-                    X = randomGenerator.Next(0, 50),
-                    Y = randomGenerator.Next(0, 50)
+                    X = (i % 6) * 10,
+                    Y = (i / 6) * 10
                 };
                 Coordinates.Add(i, coordinates);
             }
-            Coordinates.Add(vq, new Coordinates
-            {
-                X = 25,
-                Y = 17,
-            });
+            //Coordinates.Add(vq, new Coordinates
+            //{
+            //    X = 50.0,
+            //    Y = 50.0,
+            //});
 
         }
 
         private void CalculateDistance()
         {
-            for (int i = 1; i < V_size; i++)
+            for (int i = 0; i < V_size + 1; i++)
             {
-                for (int j = 1; j < V_size; j++)
+                for (int j = 0; j < V_size + 1; j++)
                 {
-                    D[i, j] = Math.Sqrt(Math.Pow((Coordinates[j].X - Coordinates[i].X), 2) + Math.Pow((Coordinates[j].Y - Coordinates[i].Y), 2));
-                    if (D[i, j] < R && i != j)
+                    D[i, j] = Math.Sqrt(Math.Pow(Coordinates[j].X - Coordinates[i].X, 2) + Math.Pow(Coordinates[j].Y - Coordinates[i].Y, 2));
+                    if (D[i, j] < R && i!=j)
                     {
                         Delta[i, j] = 1;
                         E.Add(new Node
@@ -117,50 +126,71 @@ namespace ConsoleApp1
         private void Initialize()
         {
             W = new List<int>();
-            L[vq] = 0;
-            M[vq] = 1;
-            for (var vi = 1; vi < V_size; vi++)
+            for (var vi = 0; vi < V_size; vi++)
             {
                 V.Add(vi);
-                L[vi] = 999999999999999999;
-                M[vi] = 0;
-                Prevoius[vi] = -1;
+                L[vi] = Double.PositiveInfinity;
+                M[vi] = 1.0;
+                Previous[vi] = -1;
             }
+            L[vq] = 0.0;
+            M[vq] = 1.0;
+            Previous[vq] = -1;
+            V.Add(vq);
         }
         public void FindPath()
         {
             GenerateData();
             Initialize();
-            while ((!W.Contains(vs)) && Neighbour.ContainsKey(W.LastOrDefault()) && Neighbour[W.LastOrDefault()].Any())
+            do
             {
                 int v_j = GetVj();
                 var v_i_List = new List<int>();
 
                 foreach (var node in E.Where(x => (x.To == v_j)).ToList())
                 {
-                     v_i_List.Add(node.From);
+                    v_i_List.Add(node.From);
                 }
                 foreach (var v_i in v_i_List)
                 {
-                    var x = L[v_j] + UtilityFunction(v_i, v_j);
+                    var c = CostFunction(v_i, v_j);
+                    var x = L[v_j] + c;
                     if (L[v_i] > x)
                     {
-                        L[v_i] = x;
                         M[v_i] = M[v_j] * p[v_i];
-                        if ((M[v_i] - UtilityFunction(v_i, v_j)) < 0)
+                        if ((M[v_i] - c) < 0)
                         {
-                            E.Remove(E.Where(a => a.From == v_i && a.To == v_j).FirstOrDefault());
+                            Console.WriteLine($"Utility function at node {v_i}");
+                            E.Remove(E.Where(node => (node.From == v_i && node.To == v_j)).FirstOrDefault());
+                            E.Remove(E.Where(node => (node.From == v_j && node.To == v_i)).FirstOrDefault());
                         }
                         else
                         {
-                            Prevoius[v_i] = v_j;
+                            L[v_i] = x;
+                            Previous[v_i] = v_j;
                         }
                     }
                 }
+            } while (!W.Contains(vs) && WHasNeighbour());
+            BuildPath();
+            PrintPath();
+            Visualize();
+        }
+
+        private bool WHasNeighbour()
+        {
+            if (W.Any(vi => E.Any(node => (node.To == vi) && (!W.Contains(node.From)))))
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"No neighbours :(");
+                return false;
             }
         }
 
-        private double UtilityFunction(int v_i, int v_j)
+        private double CostFunction(int v_i, int v_j)
         {
             SetLambda();
             return (Beta / Q) * (D[v_i, v_j] / Delta[v_i, v_j]) * (E_max / (E_nergy[v_j] * Lambda[v_j])) * (1 + (Gamma * Tao[v_j]));
@@ -168,7 +198,7 @@ namespace ConsoleApp1
 
         private void SetLambda()
         {
-            for (int i = 1; i < V_size; i++)
+            for (int i = 1; i < V_size + 1; i++)
             {
                 if (E_nergy[i] >= E_min)
                 {
@@ -183,25 +213,128 @@ namespace ConsoleApp1
         
         private int GetVj()
         {
-            for (int vj = 1; vj < V_size; vj++)
+            foreach (var vj in V)
             {
                 bool isLarger = false;
-                V.Remove(vj);
                 foreach (var vk in V)
                 {
-                    if (L[vj] > L[vk])
+                    if (L[vj] > L[vk] && vj != vk)
                     {
                         isLarger = true;
                     }
                 }
                 if (!isLarger)
                 {
+                    V.Remove(vj);
                     W.Add(vj);
                     return vj;
                 }
-                else V.Add(vj);
             }
             return -1;
+        }
+        private void BuildPath()
+        {
+            var vi = 0;
+            while (vi != -1)
+            {
+                Path.Add(vi);
+                vi = Previous[vi];
+            }
+        }
+        private void PrintPath()
+        {
+            var vi = 0;
+            while(vi != -1)
+            {
+                Console.WriteLine($"{vi}->");
+                vi = Previous[vi];
+            }
+        }
+
+        private void Visualize()
+        {
+            var node_count = V_size + 1;
+            var width = 6;
+            var number_format = "D2";
+            string horizontal_empty_space = "  ";
+            string horizontal_empty_space_one = " ";
+            string horizontal_link = "--";
+            string empty_space_under_number = "  ";
+            string right_slash = "/";
+            string left_slash = "\\";
+            string vertical_link = "| ";
+            for(int i = 0; i < width; i++)
+            {
+                var line = "";
+                var start_node = node_count - (i + 1) * width;
+                for(int j = 0; j < width;j++)
+                {
+                    var current_node = start_node + j;
+                    line = String.Concat(line, current_node.ToString(number_format));
+                    if(j!=width-1)
+                    {
+                        if (IsNeighbourPath(current_node,current_node + 1))
+                            line = String.Concat(line, horizontal_link);
+                        else
+                            line = String.Concat(line, horizontal_empty_space);
+                    }
+                }
+                Console.WriteLine(line);
+                if (i != width - 1)
+                {
+                    var line2 = "";
+                    for (int j = 0; j < width; j++)
+                    {
+                        var current_node = start_node + j;
+                        if (IsNeighbourPath(current_node,current_node-width))
+                            line2 = String.Concat(line2, vertical_link);
+                        else
+                            line2 = String.Concat(line2, empty_space_under_number);
+                        if (j != width - 1)
+                        {
+                            if (IsNeighbourPath(current_node, current_node - (width - 1)))
+                                line2 = String.Concat(line2, left_slash);
+                            else
+                                line2 = String.Concat(line2, horizontal_empty_space_one);
+                            if (IsNeighbourPath(current_node + 1, current_node - width))
+                                line2 = String.Concat(line2, right_slash);
+                            else
+                                line2 = String.Concat(line2, horizontal_empty_space_one);
+                        }
+                    }
+                    Console.WriteLine(line2);
+                    var line3 = "";
+                    for (int j = 0; j < width; j++)
+                    {
+                        var current_node = start_node + j;
+                        if (IsNeighbourPath(current_node, current_node - width))
+                            line3 = String.Concat(line3, vertical_link);
+                        else
+                            line3 = String.Concat(line3, empty_space_under_number);
+                        if (j != width - 1)
+                        {
+                            if (IsNeighbourPath(current_node + 1, current_node - width))
+                                line3 = String.Concat(line3, right_slash);
+                            else
+                                line3 = String.Concat(line3, horizontal_empty_space_one);
+                            if (IsNeighbourPath(current_node, current_node - (width - 1)))
+                                line3 = String.Concat(line3, left_slash);
+                            else
+                                line3 = String.Concat(line3, horizontal_empty_space_one);
+                        }
+                    }
+                    Console.WriteLine(line3);
+                }
+            }
+        }
+        private bool IsNeighbourPath(int v_i, int v_j)
+        {
+            if(Path.Contains(v_i) && Path.Contains(v_j))
+            {
+                if (Math.Abs(Path.IndexOf(v_i) - Path.IndexOf(v_j)) == 1)
+                    return true;
+            }
+            return false;
         }
     }
 }
